9f126fa7c87bd7b35849f0dce71ae54f4fd6fef5
Kyle Billiot <kylelukej@gmail.com>
2017-08-09 09:00:25 -0600

    Added GameBoyInput wrapper, added XInput support.

---
+++ Emux.GameBoy/Input/GameBoyInputDefinition.cs
@@ -0,0 +1,32 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Emux.GameBoy.Input
+{
+    public class GameBoyInputDefinition
+    {
+        public bool IsPressed
+        {
+            get
+            {
+                return _key != Key.None ? Keyboard.IsKeyDown(_key) : XInputUtil.IsPressed(_flag);
+            }
+        }
+
+        private Key _key;
+        private GamepadButtonFlags _flag;
+
+        public GameBoyInputDefinition(Key key) => _key = key;
+        public GameBoyInputDefinition(GamepadButtonFlags flag) => _flag = flag;
+
+        public override string ToString()
+        {
+            return _key != Key.None ? _key.ToString() : _flag.ToString();
+        }
+    }
+}
--- Emux.GameBoy/Input/IoManager.cs
+++ Emux.GameBoy/Input/IoManager.cs
@@ -12,16 +12,17 @@ namespace Emux.GameBoy.Input
 {
     public class IoManager
     {
-        public static Dictionary<GameBoyPadButton, Key> InputMap = new Dictionary<GameBoyPadButton, Key>()
+
+        public static Dictionary<GameBoyPadButton, GameBoyInputDefinition> InputMap = new Dictionary<GameBoyPadButton, GameBoyInputDefinition>()
         {
-            {Up,     Key.Up },
-            {Down,   Key.Down },
-            {Left,   Key.Left },
-            {Right,  Key.Right },
-            {A,      Key.X },
-            {B,      Key.Z },
-            {Start,  Key.Enter },
-            {Select, Key.LeftShift },
+            {Up,     new GameBoyInputDefinition(Key.Up)        },
+            {Down,   new GameBoyInputDefinition(Key.Down)      },
+            {Left,   new GameBoyInputDefinition(Key.Left)      },
+            {Right,  new GameBoyInputDefinition(Key.Right)     },
+            {A,      new GameBoyInputDefinition(Key.X)         },
+            {B,      new GameBoyInputDefinition(Key.Z)         },
+            {Start,  new GameBoyInputDefinition(Key.Enter)     },
+            {Select, new GameBoyInputDefinition(Key.LeftShift) },
         };
 
         public static ManualResetEvent IoBlockEvent = new ManualResetEvent(true);
@@ -35,6 +36,7 @@ namespace Emux.GameBoy.Input
             ioThread = new Thread(Update);
             ioThread.SetApartmentState(ApartmentState.STA);
             ioThread.Start();
+            XInputUtil.StartThread();
         }
 
         public void StopThread()
@@ -42,7 +44,6 @@ namespace Emux.GameBoy.Input
             ioThread.Abort();
         }
 
-        long t = 0;
         public void Update()
         {
             while (true)
@@ -50,8 +51,7 @@ namespace Emux.GameBoy.Input
                 IoBlockEvent.WaitOne();
                 foreach (GameBoyPadButton k in InputMap.Keys)
                 {
-                    Console.WriteLine(t++);
-                    if (IsKeyDown(InputMap[k])) vm.KeyPad.PressedButtons |= k;
+                    if (InputMap[k].IsPressed) vm.KeyPad.PressedButtons |= k;
                     else vm.KeyPad.PressedButtons &= ~k;
                 }
             }
